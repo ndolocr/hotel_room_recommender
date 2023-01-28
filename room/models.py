4d6e152c8d7032725b70d3f08e0c545c71a022a4
ndolocr <crndolo@gmail.com>
2023-01-28 11:29:03 +0300

    Creating model for Room Elelemnt

--- room/models.py
+++ room/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class RoomElement(models.Model):
+    name = models.CharField(max_length=255, null=True)
+    description = models.TextField(null=True)
+    elementType = models.CharField(max_length=255, null=True)
+
+    updated_on = models.DateTimeField( auto_now = True)
+    created_on = models.DateTimeField( auto_now_add = True)
+
+    def __str__(self):
+        return self.name
