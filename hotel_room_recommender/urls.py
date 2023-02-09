bd272f51aa6e06cf0dd7762c66e707bd21a93ee7
ndolocr <crndolo@gmail.com>
2023-02-09 15:59:25 +0300

    Creating bas template file

--- hotel_room_recommender/urls.py
+++ hotel_room_recommender/urls.py
@@ -14,11 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-from core. views import index, dashboard
+from django.urls import path
+from core.views import index
+from django.urls import include
+from core.views import dashboard
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name="login-page"),
-    path('dashboard', dashboard, name=dashboard),
+    path('dashboard/', dashboard, name="dashboard"),
 ]
