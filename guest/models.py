93fec55e9154f84c3d1b4ab9a6d400690f33867b
ndolocr <crndolo@gmail.com>
2023-02-17 23:00:14 +0300

    Creating model for Guest

--- guest/models.py
+++ guest/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from neomodel import(
+    Relationship,
+    StringProperty,
+    StructuredNode,
+    RelationshipTo,
+    RelationshipFrom,
+    UniqueIdProperty,
+    IntegerProperty,
+    ArrayProperty, 
+    DateProperty
+)
 
 # Create your models here.
+class Guest(StructuredNode):
+    name = StringProperty()
+    title = StringProperty()
+    gender = StringProperty()
+    address = StringProperty()
+    religion = StringProperty()
+    date_of_birth = DateProperty()
+    nationality = StringProperty()
+    phone_number = StringProperty()
+    marital_status = StringProperty()
+    country_of_residence = StringProperty()
+    email = StringProperty(unique_index=True)    
+    id_num = StringProperty(unique_index=True)
+
+
+    
+    
+
