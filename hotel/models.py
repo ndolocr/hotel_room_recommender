6e8e59cd75a8f5ddd4663815ce44f0c0aa8ac206
ndolocr <crndolo@gmail.com>
2023-02-17 22:33:40 +0300

    Recreating model for hotel information

--- hotel/models.py
+++ hotel/models.py
@@ -1,14 +1,34 @@
 from django.db import models
+from neomodel import (
+    Relationship,
+    StringProperty,
+    StructuredNode,
+    RelationshipTo,
+    RelationshipFrom,
+    UniqueIdProperty,
+    IntegerProperty,
+    ArrayProperty, 
+    DateProperty
+)
 
 # Create your models here.
-class Hotel(models.Model):
-    name = models.CharField(max_length=255, null=True)
-    description = models.TextField(null=True)
-    city = models.CharField(max_length=255, null=True)
-    address = models.CharField(max_length=255, null=True)
+class Hotel(StructuredNode):
+    name = StringProperty()
+    city = StringProperty()
+    address = StringProperty()
+    description = StringProperty()
 
-    updated_on = models.DateTimeField( auto_now = True)
-    created_on = models.DateTimeField( auto_now_add = True)
+    created_on = DateProperty()
 
-    def __str__(self):
-        return self.name
+
+# class Hotel(models.Model):
+    # name = models.CharField(max_length=255, null=True)
+    # description = models.TextField(null=True)
+    # city = models.CharField(max_length=255, null=True)
+    # address = models.CharField(max_length=255, null=True)
+
+    # updated_on = models.DateTimeField( auto_now = True)
+    # created_on = models.DateTimeField( auto_now_add = True)
+
+    # def __str__(self):
+    #     return self.name
