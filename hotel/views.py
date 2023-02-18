4b6b0a14d71b60ce6493f47cdc9064446cb677ce
ndolocr <crndolo@gmail.com>
2023-02-18 08:24:08 +0300

    Include condition to check if adding a hotel is a post request

--- hotel/views.py
+++ hotel/views.py
@@ -9,28 +9,29 @@ from hotel.models import Hotel
 # HOTEL
 @csrf_exempt
 def addHotel(request):
-    json_data = json.loads(request.body)
+    if request.method == "POST":
+        json_data = json.loads(request.body)
 
-    try:
-        hotel = Hotel(
-            name = json_data["name"],
-            city = json_data["city"],
-            address = json_data["address"],
-            description = json_data["description"],
-            created_on = datetime.today()
-        )
+        try:
+            hotel = Hotel(
+                name = json_data["name"],
+                city = json_data["city"],
+                address = json_data["address"],
+                description = json_data["description"],
+                created_on = datetime.today()
+            )
 
-        hotel.save()
-        response = {
-            "uid" : hotel.uid,
-            "name" : hotel.name,
-            "city" : hotel.city,
-            "address" : hotel.address,
-            "description" : hotel.description,
-            "created_on" : hotel.created_on
-        }
+            hotel.save()
+            response = {
+                "uid" : hotel.uid,
+                "name" : hotel.name,
+                "city" : hotel.city,
+                "address" : hotel.address,
+                "description" : hotel.description,
+                "created_on" : hotel.created_on
+            }
 
-        return JsonResponse(response, safe=False)
-    except Exception as e:
-        response = { "Error": "Error adding Hotel information - {}".format(e)}
-        return JsonResponse(response, safe=False)
+            return JsonResponse(response, safe=False)
+        except Exception as e:
+            response = { "Error": "Error adding Hotel information - {}".format(e)}
+            return JsonResponse(response, safe=False)
