0e37e28ee9bf50cba5e383cafd77297a0b9b34cc
ndolocr <crndolo@gmail.com>
2023-02-18 07:42:20 +0300

    Remove beginning slash to url patterns

--- room/urls.py
+++ room/urls.py
@@ -3,11 +3,11 @@ from room import views
 
 urlpatterns = [
     path('', views.getAllRooms, name='get-all-rooms'),
-    path('/add', views.addRoom, name='room-details'),
-    path('/hotel/add', views.addHotel, name='hotel-add'),
-    path('/element/add', views.addRoomElement, name='element-add'),
-    path('/room/type/add', views.addRoomType, name='room-type-add'),
-    path('/connect/to/hotel', views.connectHotelToRoom, name="connect-hotel-to-room"),
-    path('/connect/to/room/type', views.connectRoomTypeToRoom, name="connect-room-type-to-room"),
-    path('/connect/to/room/element', views.connectRoomElelemntToRoom, name="connect-room-element-to-room"),
+    path('add', views.addRoom, name='room-details'),
+    path('hotel/add', views.addHotel, name='hotel-add'),
+    path('element/add', views.addRoomElement, name='element-add'),
+    path('room/type/add', views.addRoomType, name='room-type-add'),
+    path('connect/to/hotel', views.connectHotelToRoom, name="connect-hotel-to-room"),
+    path('connect/to/room/type', views.connectRoomTypeToRoom, name="connect-room-type-to-room"),
+    path('connect/to/room/element', views.connectRoomElelemntToRoom, name="connect-room-element-to-room"),
 ]
