1711c0e316b14bf38b1d0bc80b3a4be57fc7e00d
ndolocr <crndolo@gmail.com>
2023-02-18 09:06:35 +0300

    Creating urls for getting single and all hotel records

--- hotel/urls.py
+++ hotel/urls.py
@@ -4,6 +4,8 @@ from hotel import views
 
 urlpatterns = [
     path('add', views.addHotel, name='hotel-add'),
+    path('get/all', views.getAllHotels, name='hotel-get-all'),
     path('update/<str:uid>', views.updateHotel, name='hotel-update'),
     path('delete/<str:uid>', views.deleteHotel, name='hotel-delete'),
+    path('get/single/<str:uid>', views.getSingleHotel, name='hotel-get-single'),
 ]
