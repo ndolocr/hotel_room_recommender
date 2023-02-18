b91311b658eaf5c03e58e5cecbb49a9a6614b995
ndolocr <crndolo@gmail.com>
2023-02-18 07:53:18 +0300

    Include model properties for guest

--- guest/models.py
+++ guest/models.py
@@ -13,55 +13,55 @@ from neomodel import(
 
 # Create your models here.
 class Guest(StructuredNode):
-    name = StringProperty()
-    title = StringProperty()
-    gender = StringProperty()
-    address = StringProperty()
-    religion = StringProperty()
-    date_of_birth = DateProperty()
-    nationality = StringProperty()
-    phone_number = StringProperty()
-    marital_status = StringProperty()
-    country_of_residence = StringProperty()
-    email = StringProperty(unique_index=True)    
-    id_num = StringProperty(unique_index=True)
+    name = StringProperty(required = True)
+    title = StringProperty(required = True)
+    gender = StringProperty(required = True)
+    address = StringProperty(required = True)
+    religion = StringProperty(required = True)
+    date_of_birth = DateProperty(required = True)
+    nationality = StringProperty(required = True)
+    phone_number = StringProperty(required = True)
+    marital_status = StringProperty(required = True)
+    country_of_residence = StringProperty(required = True)
+    email = StringProperty(unique_index=True, required = True)    
+    id_num = StringProperty(unique_index=True, required = True)
     
     relaton_to_guest = StringProperty()
     guest = RelationshipTo('Guest', 'Guest of')
 
 class childGuest(StructuredNode):
-    name = StringProperty()
-    gender = StringProperty()
-    date_of_birth = DateProperty()
+    name = StringProperty(required = False)
+    gender = StringProperty(required = False)
+    date_of_birth = DateProperty(required = False)
 
     guest = RelationshipTo(Guest, 'Child of')
 
 class Pet(StructuredNode):
-    name = StringProperty()
-    pet_type = StringProperty()
+    name = StringProperty(required = False)
+    pet_type = StringProperty(required = False)
 
     guest = RelationshipTo(Guest, 'Pet of Guest')
 
 class GuestAttributes(StructuredNode):
-    is_smoker = StringProperty()
-    health_condition = StringProperty()
+    is_smoker = StringProperty(required = False)
+    health_condition = StringProperty(required = False)
 
     guest = RelationshipTo(Guest, 'Attributes of Guest')
     
 class Allergy(StructuredNode):
-    name = StringProperty()
-    description = StringProperty()
+    name = StringProperty(required = False)
+    description = StringProperty(required = False)
     guest = RelationshipTo(Guest, 'Guest allergy')
 
 class Disability(StructuredNode):
-    name = StringProperty()
-    disability_type = StringProperty()
+    name = StringProperty(required = False)
+    disability_type = StringProperty(required = False)
     diasability_description = StringProperty()
 
     guest = RelationshipTo(Guest, 'Guest disability')
 
 class Illness(StructuredNode):
-    name = StringProperty()
-    description = StringProperty()
+    name = StringProperty(required = False)
+    description = StringProperty(required = False)
 
     guest = RelationshipTo(Guest, 'Guest suffers from')
