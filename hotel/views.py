818dc4f6129df76e682331608b5cd56fba7fc67d
ndolocr <crndolo@gmail.com>
2023-02-18 08:21:10 +0300

    Create function to add hotel in hotel app view

--- hotel/views.py
+++ hotel/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+import json
+from datetime import datetime
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 
+from hotel.models import Hotel
 # Create your views here.
+# ***********************************************************************************
+# HOTEL
+@csrf_exempt
+def addHotel(request):
+    json_data = json.loads(request.body)
+
+    try:
+        hotel = Hotel(
+            name = json_data["name"],
+            city = json_data["city"],
+            address = json_data["address"],
+            description = json_data["description"],
+            created_on = datetime.today()
+        )
+
+        hotel.save()
+        response = {
+            "uid" : hotel.uid,
+            "name" : hotel.name,
+            "city" : hotel.city,
+            "address" : hotel.address,
+            "description" : hotel.description,
+            "created_on" : hotel.created_on
+        }
+
+        return JsonResponse(response, safe=False)
+    except Exception as e:
+        response = { "Error": "Error adding Hotel information - {}".format(e)}
+        return JsonResponse(response, safe=False)
