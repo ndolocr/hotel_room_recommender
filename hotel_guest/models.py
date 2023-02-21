09f450820cb168eeedeff672ef102c96e94ca45a
ndolocr <crndolo@gmail.com>
2023-02-22 01:04:51 +0300

    Creating the Guest model

--- hotel_guest/models.py
+++ hotel_guest/models.py
@@ -2,4 +2,23 @@ from django.db import models
 
 # Create your models here.
 class HotelGuest(models.Model):
+    title = models.CharField(max_length=255, null=True)
+    gender = models.CharField(max_length=255, null=True)
+    address = models.CharField(max_length=255, null=True)
+    religion = models.CharField(max_length=255, null=True)
+    id_number = models.CharField(max_length=255, null=True)
+    residency = models.CharField(max_length=255, null=True)
+    last_name = models.CharField(max_length=255, null=True)
+    first_name = models.CharField(max_length=255, null=True)
+    middle_name = models.CharField(max_length=255, null=True)    
+    nationality = models.CharField(max_length=255, null=True)
+    phone_number = models.CharField(max_length=255, null=True)
+    email_address = models.CharField(max_length=255, null=True)
+    marital_status = models.CharField(max_length=255, null=True)
+    
+    dob = models.DateTimeField(auto_now=False)
+    updated_on = models.DateTimeField(auto_now=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    
+
     
