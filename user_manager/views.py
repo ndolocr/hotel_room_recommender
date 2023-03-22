eb0221030309441af33bed75efb27757c4009256
ndolocr <crndolo@gmail.com>
2023-03-22 23:37:15 +0300

    Creating view to load regictration page"

--- user_manager/views.py
+++ user_manager/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+def register(request):
+    if request.method == "GET":
+        context = {}
+        return render(request, 'user_manager/register.html', context = context)
+    else:
+        pass
