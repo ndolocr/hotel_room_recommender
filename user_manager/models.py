d801d031b405b21ba209f3eb1b5f8c091d04df69
ndolocr <crndolo@gmail.com>
2023-03-22 22:34:34 +0300

    Creatung use model manager class

--- user_manager/models.py
+++ user_manager/models.py
@@ -1,8 +1,47 @@
 from django.db import models
+from django.urls import reverse
 from django.contrib.auth.models import PermissionsMixin, User
 from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
 
 # Create your models here.
+
+class UserModuleManager(BaseUserManager):
+
+    def _create_user(self, email, password, **extra_fields):
+        """
+        Creates and saves a User with the given email and password.
+        email, password, False, **data
+        """
+        if not email:
+            raise ValueError('The Email must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+
+        user.is_active=True
+        user.set_password(password)
+        
+        user.save()
+        created = True
+
+        return user, created
+
+    def create_superuser(self, email, password, **extra_fields):
+        # extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+
+        # if extra_fields.get('is_staff') is not True:
+        # raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+        return self._create_user(email, password, **extra_fields)
+
+    def __str__(self):
+        return self.email
+
+    def get_absolute_url(self):
+        return reverse('user_manager', kwargs={'pk': self.pk})
+    
 class User(AbstractBaseUser, PermissionsMixin):
     """creates a usermodel that supports email address instead of username"""
 
@@ -241,4 +280,4 @@ class User(AbstractBaseUser, PermissionsMixin):
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
     
-    
+    objects = UserModuleManager()
