5a68798424ae613ae58f70761daea8c3e8bc9be4
ndolocr <crndolo@gmail.com>
2023-03-23 00:40:21 +0300

    Start creation of view to save user registration information

--- user_manager/views.py
+++ user_manager/views.py
@@ -6,4 +6,24 @@ def register(request):
         context = {}
         return render(request, 'user_manager/register.html', context = context)
     else:
-        pass
+        password = request.POST["password"]
+        confirm_password = request.POST["confirm_password"]
+
+        if password == confirm_password:
+            dob = request.POST['dob']+"T00:00:00.000000"
+            town = request.POST['town']
+            title = request.POST['title']
+            gender = request.POST['gender']
+            address = request.POST['address']
+            religion = request.POST['religion']
+            area_code = request.POST['area_code']
+            residency = request.POST['residency']
+            last_name = request.POST['last_name']
+            first_name = request.POST['first_name']
+            middle_name = request.POST['middle_name']
+            nationality = request.POST['nationality']
+            phone_number = request.POST['phone_number']
+            email_address = request.POST['email_address']
+            marital_status = request.POST['marital_status']
+            id_document_type = request.POST['id_document_type']
+            id_document_number = request.POST['id_document_number']
