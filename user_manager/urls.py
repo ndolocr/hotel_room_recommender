7b5b65498451d6ebe252b514ed57125af7855911
ndolocr <crndolo@gmail.com>
2023-03-23 23:35:51 +0300

    Include url for user registration and logging in

--- user_manager/urls.py
+++ user_manager/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from user_manager import views
 urlpatterns = [
-    path('register', views.register, name='user-registration'),
+    path('home', views.login, name="login"),
+    path('register', views.register, name='user-registration'),    
 ]
