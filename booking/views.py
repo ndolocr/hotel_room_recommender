709cdbcdb38f92a9a5573cfe155f66d5188476a2
ndolocr <crndolo@gmail.com>
2023-03-26 01:39:32 +0300

    Include room temprature in booking context

--- booking/views.py
+++ booking/views.py
@@ -11,7 +11,8 @@ from user_manager.models import UserManager
 
 from room.models import RoomType
 from room.models import RoomElement
-from room.views import RoomViewPreference
+from room.models import RoomTemprature
+from room.models import RoomViewPreference
 
 # Create your views here.
 def booking(request):
@@ -95,9 +96,10 @@ def booking_self(request):
         room_type_data = {
             "uid" : room_type.uid,
             "Name": room_type.name,
-            "Maximum_Capacity": room_type.max_capacity,
+            "room_type_uid": room_type.uid,
             "created_on": room_type.created_on,
             "Description": room_type.description,
+            "Maximum_Capacity": room_type.max_capacity,
         }
 
         room_type_response.append(room_type_data)
@@ -108,8 +110,9 @@ def booking_self(request):
     room_views = RoomViewPreference.nodes.all()
 
     for room_view in room_views:
-        room_view_data = {
+        room_view_data = {            
             "name": room_view.name,
+            "room_view_uid": room_view.uid,
             "description": room_view.description,
         }
 
@@ -121,13 +124,27 @@ def booking_self(request):
     room_elements = RoomElement.nodes.all()
     
     for room_element in room_elements:
-        room_elements_data = {
+        room_elements_data = {            
             "name": room_element.name,
+            "room_element_uid": room_element.uid,
             "description": room_element.description,
             "elementType": room_element.elementType,
         }
         room_elements_response.append(room_elements_data)
     # end room elements
+
+    # begin room temprature range
+    room_temprature_response = []
+    room_temprature = RoomTemprature.nodes.all()
+
+    for room_temp in room_temprature:
+        room_temprature_data = {
+            "temp_uid": room_temp.uid,
+            "max_temprature": room_temp.max_temprature,
+            "min_temprature": room_temp.min_temprature,
+        }
+        room_temprature_response.append(room_temprature_data)
+    # end room temprature range
 
     context = {
         "guest_id": guest.uid,
@@ -137,6 +154,7 @@ def booking_self(request):
         "room_view_response": room_view_response,
         "room_type_response": room_type_response,
         "room_elements_response": room_elements_response,
+        "room_temprature_response": room_temprature_response,
     }
 
     return render(request, 'core/booking_room_information.html', context=context)
