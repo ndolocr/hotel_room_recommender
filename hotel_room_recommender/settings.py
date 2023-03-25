773b76ca187b0ddb53fb11b7cadca2daeea8fb58
ndolocr <crndolo@gmail.com>
2023-03-25 15:04:25 +0300

    Include database for qslite3 to save user information

--- hotel_room_recommender/settings.py
+++ hotel_room_recommender/settings.py
@@ -144,3 +144,5 @@ MEDIA_ROOT = [ BASE_DIR / 'media', ]
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'user_manager.UserManager'
