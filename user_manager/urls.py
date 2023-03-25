ebdccee339226238cf88c32aa0d810fd5fbbfc80
ndolocr <crndolo@gmail.com>
2023-03-25 18:25:04 +0300

    Include urls for user authenitication

--- user_manager/urls.py
+++ user_manager/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from user_manager import views
+
 urlpatterns = [
     path('home', views.login, name="login"),
+    path('login_user', views.login_user, name='login_user'),
+    path('logout_user', views.logout_user, name='logout_user'),
     path('register', views.register, name='user-registration'),    
 ]
