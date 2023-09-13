ab3b67acafcfe7fad09ef3f5f412cab5c411e7ac
ndolocr <crndolo@gmail.com>
2023-09-13 14:06:47 +0300

    Created model for score

--- score/models.py
+++ score/models.py
@@ -1,3 +1,22 @@
 from django.db import models
-
+from neomodel import (
+    Relationship,
+    StringProperty,
+    StructuredNode,
+    RelationshipTo,
+    RelationshipFrom,
+    UniqueIdProperty,
+    IntegerProperty,
+    ArrayProperty, 
+    DateProperty, 
+    FloatProperty
+)
 # Create your models here.
+
+class Score(StructuredNode):
+    score = FloatProperty()
+    uid = UniqueIdProperty()
+    scoreFor = StringProperty()
+
+    updated_on = DateProperty()
+    created_on = DateProperty()
